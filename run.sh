feb22ec2540e86a3a77d891cb94eac3cc5414929
Dominique DERRIER <derrierdo@gmail.com>
2015-10-24 21:49:19 -0400

    Add link server inventory

--- run.sh
+++ run.sh
@@ -8,7 +8,13 @@ echo "}"
 
 }
 
+function serv
+{
+env | grep _NAME | awk -F= '{print $2}'
+}
+
 run > /etc/nginx/upstream
+serv > /var/www/html/serv
 nginx
 ip a s eth0 | grep inet | awk -F'[\/\t\ ]' '/inet /{print "http://"$6"/index.php"}'
 exec /bin/bash
